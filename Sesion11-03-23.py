b9cb214815bfd1b457a9b9b6b914939143b904dd
bryan-br0n <126531322+bryan-br0n@users.noreply.github.com>
2023-03-11 19:11:02 -0600

    Ejemplos Sesion11-03

---
+++ Sesion11-03-23.py
@@ -0,0 +1,19 @@
+'''not1 = float(input("Ingrese su nota 1 -> "))
+not2 = float(input("\nIngrese su nota 2 -> "))
+not3 = float(input("\nIngrese su nota 3 -> "))
+not4 = float(input("\nIngrese su nota 4 -> "))
+
+promedio = (not1 + not2 + not3 + not4)/4
+
+if promedio >= 6:
+    print(f"\nUsted aprobo la materia con un promedio de {round(promedio, 2)}")
+
+else:
+    print("\nUsted no aprobo el curso, su promedio fue de ", promedio)
+
+print("\nFin del programa")'''
+
+fruta = input("Ingrese su fruta ->").lower()
+
+if fruta == 'sandia':
+    print("Eligio sandia")
